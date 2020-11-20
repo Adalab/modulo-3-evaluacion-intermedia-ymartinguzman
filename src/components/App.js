ee696197115a0eb8adfe75e5ed9d6fcb5817117b
ymartinguzman <yamiramartinguzman@gmail.com>
2020-11-20 18:31:13 +0100

    change data in head, index.html

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheet/App.scss';
 import pokemonArray from '../data/pokemon.json';
 import PokeList from './PokeList';
+import { unstable_concurrentAct } from 'react-dom/test-utils';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState(pokemonArray);
+  console.log({ setPokemonData });
+
   return (
     <div>
       <PokeList data={pokemonArray} />
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,19 +2,14 @@ import React from 'react';
 
 class Pokemon extends React.Component {
   render() {
-    console.log(this.props.family);
     const card = this.props.family.map((item, i) => {
-      return (
-        <li className="" key={i}>
-          {item}
-        </li>
-      );
+      return <li key={i}>{item}</li>;
     });
 
     return (
       <div className="container-border">
-        <img src={this.props.img} alt={this.props.name} className=""></img>
-        <div className="">
+        <img src={this.props.img} alt={this.props.name}></img>
+        <div>
           <h2 className="title-card">{this.props.name}</h2>
           <ul className="title-type">{card}</ul>
         </div>
